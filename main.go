344950c80da0898b545d56f513661a77436ce028
Gleb <fedorov22134@gmail.com>
2025-06-16 15:49:07 +0300

    add solution for task 2 (dirty code, but correct)

--- main.go
+++ main.go
@@ -6,7 +6,41 @@ import (
 )
 
 func main() {
-	result := tasks_from_leetcode.TwoSum([]int{2, 7, 11, 15}, 9)
+	var head *tasks_from_leetcode.ListNode
+	var current *tasks_from_leetcode.ListNode
 
-	fmt.Println(result)
+	for _, val := range []int{3, 4, 2} {
+		newNode := &tasks_from_leetcode.ListNode{Val: val}
+
+		if head == nil {
+			head = newNode
+			current = head
+		} else {
+			current.Next = newNode
+			current = current.Next
+		}
+	}
+
+	var head2 *tasks_from_leetcode.ListNode
+	var current2 *tasks_from_leetcode.ListNode
+
+	for _, val := range []int{4, 6, 5} {
+		newNode := &tasks_from_leetcode.ListNode{Val: val}
+
+		if head2 == nil {
+			head2 = newNode
+			current2 = head2
+		} else {
+			current2.Next = newNode
+			current2 = current2.Next
+		}
+	}
+
+	for current := head; current != nil; current = current.Next {
+		fmt.Println(current.Val)
+	}
+
+	for current := head2; current != nil; current = current.Next {
+		fmt.Println(current.Val)
+	}
 }
---
+++ tasks_from_leetcode/2_AddTwoNumbers.go
@@ -0,0 +1,75 @@
+package tasks_from_leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	remained := 0
+	var head *ListNode
+	var current *ListNode
+
+	for l1 != nil && l2 != nil {
+		valueL1 := l1.Val
+		valueL2 := l2.Val
+
+		l1 = l1.Next
+		l2 = l2.Next
+
+		finalValue := valueL1 + valueL2
+		if remained > 0 {
+			finalValue += remained
+			remained = 0
+		}
+		if finalValue >= 10 {
+			remained = finalValue - finalValue%10
+			if remained%10 == 0 {
+				remained = 1
+			}
+			finalValue = finalValue % 10
+		}
+
+		newNode := &ListNode{Val: finalValue}
+
+		if head == nil {
+			head = newNode
+			current = newNode
+		} else {
+			current.Next = newNode
+			current = current.Next
+		}
+	}
+
+	if l2 != nil {
+		l1 = l2
+	}
+
+	for l1 != nil {
+		finalValue := l1.Val
+		if remained > 0 {
+			finalValue += remained
+			remained = 0
+		}
+		if finalValue >= 10 {
+			remained = finalValue - finalValue%10
+			if remained%10 == 0 {
+				remained = 1
+			}
+			finalValue = finalValue % 10
+		}
+
+		newNode := &ListNode{Val: finalValue}
+		l1 = l1.Next
+
+		current.Next = newNode
+		current = current.Next
+	}
+
+	if remained > 0 {
+		current.Next = &ListNode{Val: remained}
+		current = current.Next
+	}
+
+	return head
+}
