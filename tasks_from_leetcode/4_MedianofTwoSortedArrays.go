09dc7cfc8c18d78b253aaf2a44d5351ea9055e9c
Gleb <fedorov22134@gmail.com>
2025-06-17 22:38:31 +0300

    add solution for task 4 (skip 3, in future)

---
+++ tasks_from_leetcode/4_MedianofTwoSortedArrays.go
@@ -0,0 +1,17 @@
+package tasks_from_leetcode
+
+import (
+	"sort"
+)
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	nums := append(nums1, nums2...)
+	sort.Ints(nums)
+	length := len(nums)
+
+	if length%2 != 0 {
+		return float64(nums[length/2])
+	}
+
+	return float64(nums[length/2]+nums[length/2-1]) / 2
+}
