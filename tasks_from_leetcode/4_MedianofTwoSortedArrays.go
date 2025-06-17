d13d0c5e8ab8eee87c8e6401479a4466655e2c2c
Gleb <fedorov22134@gmail.com>
2025-06-17 22:40:38 +0300

    change name func for 4 task (for import)

--- tasks_from_leetcode/4_MedianofTwoSortedArrays.go
+++ tasks_from_leetcode/4_MedianofTwoSortedArrays.go
@@ -4,7 +4,7 @@ import (
 	"sort"
 )
 
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	nums := append(nums1, nums2...)
 	sort.Ints(nums)
 	length := len(nums)
