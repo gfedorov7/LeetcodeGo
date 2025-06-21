2a2ff15dfc73c095cd3b07554dc8580a0c8ed5b3
gfedorov7 <fedorov.programming@gmail.com>
2025-06-21 19:43:31 +0300

    add solution for task 8

---
+++ tasks_from_leetcode/8_StringtoInteger_atoi.go
@@ -0,0 +1,35 @@
+package tasks_from_leetcode
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+func myAtoi(s string) int {
+	s = strings.TrimSpace(s)
+
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		last_num := len(s)
+		for idx, char := range s {
+			if idx == 0 && (string(char) == "-" || string(char) == "+") {
+				continue
+			}
+			if !strings.Contains("0123456789", string(char)) {
+				last_num = idx
+				break
+			}
+		}
+		num, _ = strconv.Atoi(s[:last_num])
+	}
+
+	if v := -math.Pow(2.0, 31.0); float64(num) < v {
+		return int(v)
+	}
+	if v := math.Pow(2.0, 31) - 1; float64(num) > v {
+		return int(v)
+	}
+
+	return num
+}
